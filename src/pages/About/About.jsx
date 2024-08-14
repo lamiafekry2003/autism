149be99dac6c80bdde9e1e732948d598e23d70be
DESKTOP-5EMQ9IB\user <Lamia.fekry2003@gmail.com>
2024-08-14 17:54:29 +0300

    chamge

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -287,8 +287,9 @@ export default function About() {
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.7 }} 
+        className=" flex justify-center items-center"
         >
-          <h2 className="text-3xl font-bold text-center mb-10 mx-2">Frequently Asked Questions</h2>
+          <h2 className="text-3xl font-bold text-center mb-10 ">Frequently Asked Questions</h2>
         </motion.div>
         <div >
           {faqs.map((faq, index) => (
